415cd532b9d5c828b0cd5a2af3b496a236bf7c26
DheVam <vanshidheeraj07@gmail.com>
2022-03-11 22:49:24 +0530

    comments added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@
 // virtual DOM is a representation of the UI
 
 import Example from "./example";
-import React from 'react';
-import './index.css';
-import {BrowserRouter,Route,Link,Routes} from 'react-router-dom';
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
@@ -19,23 +19,36 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <nav>
-         <ul>
-           <li><Link to="/">Home</Link></li>
-           <li><Link to="/about">About</Link></li>
-           <li><Link to="/contact">Contact</Link></li>
-         </ul>
-       </nav>
-       <Routes>
-         <Route exact path='/' element={<Home/>}/>
-         <Route path="/about" element={<About/>}/>
-         <Route path="/contact" element={<Contact/>}/>
-       </Routes>
-      
-
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// BrowserRouter is parent component which has stores all the other elements.
+// Everything within this BrowserRouter will be a part of routing
+
+// Routes is the parent component which holds all the components that needs to be routed
+
+// Route element hold the user-defined path and the element that needs to be routes
+
+// Link element is nothing but our <a></a> tag which hold the source link of the element to be routed when clicked
